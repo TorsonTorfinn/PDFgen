fe7160de7be47f473fd115e769ec715f178fd5fb
Сухроб Юсубахмедов <Suhrob.Yusubaxmedov@ucell.uz>
2024-09-15 12:54:54 +0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="portrait", unit="mm", format='A4')
+df = pd.read_csv('topics.csv')
+
+for index, item in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family='italic', style='B',size=24)
+    pdf.set_text_color(254, 100, 100)
+    pdf.cell(w=0, h=12, txt=item['Topic'], align='L', ln=1)
+    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+pdf.output("outout.pdf")
