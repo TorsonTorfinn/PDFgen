0791d77980709a80defed05f1f43b39ba6989f66
Сухроб Юсубахмедов <Suhrob.Yusubaxmedov@ucell.uz>
2024-09-15 13:11:19 +0500

    Added more pages for each topic.

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ df = pd.read_csv('topics.csv')
 for index, item in df.iterrows():
     pdf.add_page()
 
-    pdf.set_font(family='italic', style='B',size=24)
+    pdf.set_font(family='Courier', style='B',size=24)
     pdf.set_text_color(254, 100, 100)
     pdf.cell(w=0, h=12, txt=item['Topic'], align='L', ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for i in range(item['Pages'] - 1):
+        pdf.add_page()
 
 pdf.output("outout.pdf")
